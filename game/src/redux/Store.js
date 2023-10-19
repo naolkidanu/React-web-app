a81b4ee4d4111289920de9d0c14e9e752d927eb9
Naol Kidanu <sabelkidanu18@gmail.com>
2023-10-19 13:32:21 +0300

    Apply reducer

---
+++ game/src/redux/Store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import gameReducer from "./Slices/gameSlice";
+
+const store = configureStore({
+  reducer: {
+    game: gameReducer,
+  },
+});
+
+export default store;
