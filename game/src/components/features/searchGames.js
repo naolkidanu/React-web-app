fe9f79af9f89e94cc70e02d1813c9b7375e1a6cf
Naol Kidanu <sabelkidanu18@gmail.com>
2023-10-19 12:58:07 +0300

    Added search game placeholder functionality

---
+++ game/src/components/features/searchGames.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BsSearch } from "react-icons/bs";
+import "../styles/SearchGames.css";
+import PropTypes from "prop-types";
+
+function SearchGames({
+  searchQuery,
+  handleSearchInputChange,
+  handleSearchSubmit,
+}) {
+  return (
+    <form id="form" onSubmit={handleSearchSubmit}>
+      <input
+        id="searchInput"
+        type="search"
+        placeholder="Search Games"
+        value={searchQuery}
+        onChange={handleSearchInputChange}
+        autoComplete="off"
+      />
+      <BsSearch />
+    </form>
+  );
+}
+
+SearchGames.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  handleSearchInputChange: PropTypes.func.isRequired,
+  handleSearchSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchGames;
